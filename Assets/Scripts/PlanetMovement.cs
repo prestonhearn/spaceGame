34c1949d30a9c3439ddb57f1cf823057cd1baef4
Stickolas <nick95th@gmail.com>
2025-07-23 15:05:24 -0400

    Cleaned up Planet Move left script

--- Assets/Scripts/PlanetMovement.cs
+++ Assets/Scripts/PlanetMovement.cs
@@ -4,28 +4,30 @@ public class MoveLeft : MonoBehaviour
   
 {
 
-    public float maxspeed = 25f;
-    public float minspeed = 1f;
-    public float speed = 10f; // Speed of the planet
-    public Rigidbody2D planet1; 
-    public float offscreenx = -25; // x position where the planet is considered offscreen
-    public float[] yrange = { -3, -1, 1, 3 };// possib y positions for the planet
+
+    public float speed = 10f; 
+    public Rigidbody2D planet; 
+    public float offScreenX = -20; // x position where the planet is considered offscreen
+    public int spawnX = 15; // x position where the planet will respawn
+    public int maxY = 5; 
+    public int minY = -5; 
 
 
-                            // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        planet1 = GetComponent<Rigidbody2D>();
-        planet1.linearVelocity = new Vector2(-speed,0);
+        planet = GetComponent<Rigidbody2D>();
+        planet.linearVelocity = new Vector2(-speed,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (planet1.position.x < offscreenx)
+        if (planet.position.x < offScreenX)
         {             // Reset the planet's position to the right side of the screen
-            planet1.position = new Vector2(15, Random.Range(0,yrange.Length));
-            planet1.linearVelocity = new Vector2(-speed, 0);
+            planet.position = new Vector2(spawnX, Random.Range(maxY,minY));
+    
         }
        
     }
